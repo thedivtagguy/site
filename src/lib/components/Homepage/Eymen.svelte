ffcd8e883e929b1d201a6bfc22bc856cb2fbde6e
thedivtagguy <amanbhargava2001@gmail.com>
2023-11-29 08:13:20 +0530

    Try out enhanced image

--- src/lib/components/Homepage/Eymen.svelte
+++ src/lib/components/Homepage/Eymen.svelte
@@ -1,22 +1,7 @@
 <script>
-	import slide1 from '$lib/assets/images/common/eymen-1.png';
-	import slide2 from '$lib/assets/images/common/eymen-2.png';
-	import { onMount } from 'svelte';
-	let currentImage = slide1;
-	let images = [slide1, slide2];
-	let currentIndex = 0;
-
-	const switchImage = () => {
-		currentIndex = (currentIndex + 1) % images.length;
-		currentImage = images[currentIndex];
-	};
-
-	onMount(() => {
-		const interval = setInterval(switchImage, 2000);
-		return () => clearInterval(interval);
-	});
+	import slide1 from '$lib/assets/images/common/eymen-1.png?enhanced';
 </script>
 
 <figure class="max-w-2xl">
-	<img src={currentImage} alt="Slideshow " />
+	<enhanced:img src={slide1} alt="Slideshow " />
 </figure>
