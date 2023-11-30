53a30cdcb0940ed9fe1abe71e7122da6f8c8ca38
thedivtagguy <amanbhargava2001@gmail.com>
2023-11-30 09:55:21 +0530

    Add back enhanced

--- src/lib/components/Homepage/Eymen.svelte
+++ src/lib/components/Homepage/Eymen.svelte
@@ -1,11 +1,11 @@
 <script>
-	import slide1 from '$lib/assets/images/common/eymen-1.webp?enhanced';
-	import slide2 from '$lib/assets/images/common/eymen-2.webp?enhanced';
+	import slide1 from '$lib/assets/images/common/eymen-1.png?enhanced';
+	import slide2 from '$lib/assets/images/common/eymen-2.png?enhanced';
 	import { onMount } from 'svelte';
 	let currentImage = slide1;
 	let images = [slide1, slide2];
 	let currentIndex = 0;
-	let imageSize = '500'; // Set size for square images
+	let imageSize = '500';
 
 	const switchImage = () => {
 		currentIndex = (currentIndex + 1) % images.length;
