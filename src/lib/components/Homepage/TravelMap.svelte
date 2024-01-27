e950269c7842ccf7d41e0d57be66d1bb89f4a64e
thedivtagguy <amanbhargava2001@gmail.com>
2024-01-27 23:29:22 +0530

    Update map style

--- src/lib/components/Homepage/TravelMap.svelte
+++ src/lib/components/Homepage/TravelMap.svelte
@@ -1,5 +1,9 @@
 <script>
 	import { MapLibre } from 'svelte-maplibre';
+	const bounds = [
+		[77.314911, 12.691253],
+		[77.858734, 13.214545]
+	];
 </script>
 
 <MapLibre
@@ -7,11 +11,16 @@
 	maxZoom={12}
 	minZoom={12}
 	class="map"
+	attributionControl={false}
+	pitch={40}
+	maxBounds={bounds}
 	style="https://amanbh.netlify.app/osm_liberty.json"
 />
 
 <style>
 	:global(.map) {
 		height: 800px;
+		margin-top: -15rem;
+		margin-bottom: 2rem;
 	}
 </style>
