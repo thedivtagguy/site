6ccd376661a936a31afab73477eb9cbf470d4ea3
thedivtagguy <amanbhargava2001@gmail.com>
2024-02-02 03:05:09 +0530

    Add prerender to new APIs

--- src/routes/api/online/+server.js
+++ src/routes/api/online/+server.js
@@ -1,5 +1,5 @@
 import { ONLINE_TRACKER } from '$env/static/private';
-
+export const prerender = true;
 export async function GET() {
 	const response = await fetch(ONLINE_TRACKER);
 	const data = await response.json();
