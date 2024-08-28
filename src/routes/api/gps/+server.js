4f77f8d646e89f1a74b45277ec2fd760bd32ec12
mvatech-hub <mahavikasaghaditech@gmail.com>
2024-08-28 13:26:28 +0530

    update loc api

--- src/routes/api/gps/+server.js
+++ src/routes/api/gps/+server.js
@@ -4,9 +4,23 @@ export async function GET() {
 	const response = await fetch(GPS_LAST_LOCATION_API);
 	const data = await response.json();
 
-	const fullData = data[0];
+	// Filter for device 5894
+	const device = data.find((item) => item.device === '5894');
+
+	if (!device) {
+		return new Response(JSON.stringify({ error: 'Device 5894 not found' }), {
+			status: 404,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	}
+
 	const selectedData = {
-		batt: fullData.batt
+		batt: device.batt,
+		// lat: device.lat,
+		// lon: device.lon,
+		tst: device.tst
 	};
 
 	return new Response(JSON.stringify(selectedData), {
