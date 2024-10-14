4ddc15645715da4c931415d5864927c440eca1d2
thedivtagguy <amanbhargava2001@gmail.com>
2024-10-14 16:21:53 +0530

    adds og route

---
+++ src/lib/components/Primary/ShareCard.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { formatDate } from '$lib/utils';
+
+	export let title = 'A new post';
+	export let date = new Date().toISOString();
+</script>
+
+<div class="flex flex-row flex-col w-full col-span-1 gap-2 md:col-span-2">
+	<div class="note basis-1/2">
+		<figure class="relative max-w-sm">
+			<div
+				class="absolute w-[15rem] lg:max-w-[14rem] lg:w-full top-[15%] lg:top-[16%] flex flex-col justify-between left-[16%] -rotate-[16deg]"
+			>
+				<p class="my-2 text-sm tracking-tighter uppercase md:text-md font-fira">Blog /</p>
+				<h3 class="text-3xl uppercase break-words font-libre-caslon">
+					{title}
+				</h3>
+				<p class="my-2 text-xs tracking-tighter text-left uppercase md:m-2 md:text-md font-fira">
+					{formatDate(date)}
+				</p>
+			</div>
+			<!-- <img src={orangeNote} alt="background" height="355" class="w-full pointer-events-none" /> -->
+		</figure>
+	</div>
+</div>
+
+<style>
+	div {
+		font-family: 'Libre Caslon Condensed';
+		display: flex;
+		flex-direction: column;
+		font-size: 50px;
+
+		height: 100%;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
---
+++ src/routes/og/+server.js
@@ -0,0 +1,45 @@
+import satori from 'satori';
+import { Resvg } from '@resvg/resvg-js';
+import NotoSans from '$lib/fonts/LibreCaslonCondensed-Bold.ttf';
+import { html as toReactNode } from 'satori-html';
+import Card from '$lib/components/Primary/ShareCard.svelte';
+import { read } from '$app/server';
+
+const fontData = read(NotoSans).arrayBuffer();
+
+const height = 630;
+const width = 1200;
+
+/** @type {import('./$types').RequestHandler} */
+export const GET = async ({ url }) => {
+	const message = url.searchParams.get('message') ?? undefined;
+	const result = Card.render({ message });
+	const element = toReactNode(`${result.html}<style>${result.css.code}</style>`);
+
+	const svg = await satori(element, {
+		fonts: [
+			{
+				name: 'Libre Caslon Condensed',
+				data: await fontData,
+				style: 'normal'
+			}
+		],
+		height,
+		width
+	});
+
+	const resvg = new Resvg(svg, {
+		fitTo: {
+			mode: 'width',
+			value: width
+		}
+	});
+
+	const image = resvg.render();
+
+	return new Response(image.asPng(), {
+		headers: {
+			'content-type': 'image/png'
+		}
+	});
+};
