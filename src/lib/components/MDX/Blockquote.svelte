cd01742917a6bb0fe24ff99208e08d126f09d839
thedivtagguy <amanbhargava2001@gmail.com>
2025-01-19 13:28:35 +0530

    chore: update lockfile

---
+++ src/lib/components/MDX/Blockquote.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { TinaMarkdown } from 'tinacms/dist/rich-text';
+
+	export let children;
+</script>
+
+<blockquote class="p-4 my-4 border-l-4 border-primary bg-base-200">
+	<TinaMarkdown content={children} />
+</blockquote>
---
+++ src/lib/components/MDX/KeyTakeaway.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { TinaMarkdown } from 'tinacms/dist/rich-text';
+
+	export let children;
+</script>
+
+<div class="p-4 my-4 border-2 border-primary rounded-lg">
+	<h4 class="mb-2 font-bold">Key Takeaway</h4>
+	<TinaMarkdown content={children} />
+</div>
